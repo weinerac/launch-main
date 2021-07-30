febe6b24747ef3e3e778df1bf54dadc21a5d8592
Alex Weiner <weiner.ac@gmail.com>
2021-07-30 14:33:21 +1000

    Eslint Added

--- lesson_2/calc.js
+++ lesson_2/calc.js
@@ -1,32 +1,19 @@
 const  readline = require('readline-sync');
-
-console.log("Welocme to Calculator")
-
-
+console.log("Welocme to Calculator");
 console.log("What's the first number?");
 let num1 = readline.question();
-
-
 console.log("What's the second Number?");
-
-let num2 = readline.question()
-
+let num2 = readline.question();
 console.log("What operation would you like to perform?\n1) Add 2) Subtracy 3) Mulitply 4) Divide 5)");
 let opertaion = readline.question();
-
 let output;
 if (opertaion === '1') {
-    output = Number(num1) + Number(num2)
+  output = Number(num1) + Number(num2);
 } else if ( opertaion === '2') {
-    output = Number(num1) - Number(num2)
+  output = Number(num1) - Number(num2);
 } else if (opertaion === '3') {
-    output = Number(num1) * Number(num2)
+  output = Number(num1) * Number(num2);
 } else {
-    output = Number(num1) / Number(num2)
+  output = Number(num1) / Number(num2);
 }
-
-
 console.log(`${output}`);
-
-
-function question ()
---
+++ lesson_2/psudo.js
@@ -0,0 +1,15 @@
+// a function that returns the sum of two numbers
+// a function that takes an array of strings,
+//and returns a string that is all those strings concatenated together
+// a function that takes an array of integers, and returns a
+//new array with every other element
+
+
+//START
+// GET Number 1 from user
+// SET Number from user as num1
+// GET Number 2 from user
+// SET number form user as num2
+
+// SET sum to = num * num2
+// PRINT sum
